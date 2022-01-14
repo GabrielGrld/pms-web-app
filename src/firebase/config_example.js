a5468636e6a40152eab9d63307a099071b216dd7
GabrielGrld <gbrlgrld@gmail.com>
2022-01-14 09:26:35 -0500

    add firebase storage service to web app

--- src/firebase/config_example.js
+++ src/firebase/config_example.js
@@ -6,6 +6,7 @@ import firebase from 'firebase/app'
 
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "your-ApiKey",
@@ -22,9 +23,10 @@ const firebaseConfig = {
   // init services (init each services)
   const projectFirestore = firebase.firestore()
   const projectAuth = firebase.auth()
+  const projectStorage = firebase.storage()
 
   // timestamp
   const timestamp = firebase.firestore.Timestamp
 
-  export {projectFirestore, projectAuth, timestamp}
+  export {projectFirestore, projectAuth,projectStorage, timestamp}
 
