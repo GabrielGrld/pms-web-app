1e79460412527a63acf167a212c7b4a0b909b23e
GabrielGrld <gbrlgrld@gmail.com>
2022-01-14 09:19:55 -0500

    add image upload logic, and check siza and type of archive

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -7,11 +7,42 @@ export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword]= useState('')
   const [displayName, setDisplayName] = useState('')
-  const [thumbnail, setThumbanail] = useState(null)
+  const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailError, setThumbnailError] = useState(null)
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log (email, password, displayName, thumbnail)
+  }
+
+
+  const handleFileChange =(e) => {
+      setThumbnail(null)
+      let selected = e.target.files[0]
+      console.log(selected)
+
+      if (!selected){
+        setThumbnailError('Please select a file')
+        console.log(thumbnailError)
+        return
+      }
+      if(!selected.type.includes('image')){
+        setThumbnailError('Selected file must be an image')
+        return
+      }
+      if(selected.size>100000){
+        setThumbnailError('Image file size must be lest that 100kb')
+        return
+      }
+
+      setThumbnailError(null)
+      setThumbnail(selected)
+      console.log('thumbnail  updated')
+  }
 
     return (
         <div>
-          <form className="auth-form">
+          <form className="auth-form" onSubmit={handleSubmit}>
             <h1>Sign up</h1>
             <label>
               <span>Email:</span>
@@ -47,8 +78,9 @@ export default function Signup() {
               <input
                   required
                   type="file"
-
+                  onChange={handleFileChange}
                   />
+                  {thumbnailError &&<div className="error">{thumbnailError}</div>}
             </label>
             <button className='btn'>Sign Up </button>
 
