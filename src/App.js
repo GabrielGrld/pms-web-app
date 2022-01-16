98d18097bae55b6dde00ca4f9836e620007751dc
GabrielGrld <gbrlgrld@gmail.com>
2022-01-15 19:08:26 -0500

    add route guarding to route depending if user is logged

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 //styles
 import './App.css'
@@ -15,32 +16,43 @@ import Sidebar from './components/Sidebar';
 
 
 function App() {
+const {user, authIsReady} = useAuthContext()
+
   return (
     <div className="App">
+    {authIsReady && (
     <BrowserRouter>
     <Sidebar />
     <div className="container">
     <Navbar />      
       <Switch>
         <Route exact path="/">
-          <Dashboard />
+        {!user && <Redirect to="/login"/>}
+         {user && <Dashboard />} 
         </Route>
         <Route  path="/create">
-          <Create />
+        {!user && <Redirect to="/login"/>}
+         {user && <Create />}          
         </Route>
         <Route  path="/projects/:d">
-          <Project />
+        {!user && <Redirect to="/login"/>}
+        {user && <Project />} 
+          
         </Route>
         <Route  path="/login">
-          <Login />
+        {user && <Redirect to="/"/>}
+        {!user && <Login />}          
         </Route>
+
         <Route  path="/signup">
-          <Signup />
+        {user && <Redirect to="/"/>}
+        {!user && <Signup />}          
         </Route>
       </Switch>
     
        </div>
        </BrowserRouter>
+       )}
     </div>
   );
 }
