784f55b937ea3c0d75b3ff6e81933d14ea1c925e
GabrielGrld <gbrlgrld@gmail.com>
2022-01-16 20:36:07 -0500

    add css code to Project.css

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,9 +1,27 @@
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
+
+//components
+import ProjectSummary from './ProjectSummary'
+
+//styles
 import './Project.css'
 
+
 export default function Project() {
+    const { id } = useParams()
+    const {error, document} = useDocument('projects', id)
+
+    if (error){
+        return <div className="error">{error}</div>
+    }
+    if(!document){
+        return <div className="loading">Loading...</div>
+    }
+
     return (
-        <div>
-            <h1>Project</h1>
+        <div className="project-details">
+            <ProjectSummary project={document} />           
         </div>
     )
 }
