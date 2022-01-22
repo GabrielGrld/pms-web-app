6f8f522f7c153489521f64169e32936aacdddf2b
GabrielGrld <gbrlgrld@gmail.com>
2022-01-22 09:25:45 -0500

    add Card component to display products

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -5,7 +5,7 @@ let initialState = {
   document: null,
   isPending: false,
   error: null,
-  success: null
+  success: null,
 }
 
 const firestoreReducer = (state, action) => {
@@ -16,11 +16,10 @@ const firestoreReducer = (state, action) => {
       return { isPending: false, document: action.payload, success: true, error: null }
     case 'DELETED_DOCUMENT':
       return { isPending: false, document: null, success: true, error: null }
-    case 'UPDATED_DOCUMENT':
-      return {isPending: false, document: action.payload, success: true, error: null}
     case 'ERROR':
       return { isPending: false, document: null, success: false, error: action.payload }
-    
+    case "UPDATED_DOCUMENT":
+      return { isPending: false, document: action.payload, success: true,  error: null }
     default:
       return state
   }
@@ -33,13 +32,13 @@ export const useFirestore = (collection) => {
   // collection ref
   const ref = projectFirestore.collection(collection)
 
-  // only dispatch is not cancelled
+  // only dispatch if not cancelled
   const dispatchIfNotCancelled = (action) => {
     if (!isCancelled) {
       dispatch(action)
     }
   }
-
+  
   // add a document
   const addDocument = async (doc) => {
     dispatch({ type: 'IS_PENDING' })
@@ -67,19 +66,19 @@ export const useFirestore = (collection) => {
     }
   }
 
-  //update documents
-  const updateDocument = async (id, updates)=>{
-    dispatch({type: 'IS_PENDING'})
+  // update a document
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: "IS_PENDING" })
 
-    try{
-      const  updatedDocument = await ref.doc(id).update(updates)
-      dispatchIfNotCancelled({ type: 'UPDATED_DOCUMENT', payload:updatedDocument })
+    try {
+      const updatedDocument = await ref.doc(id).update(updates)
+      dispatchIfNotCancelled({ type: "UPDATED_DOCUMENT", payload: updatedDocument })
       return updatedDocument
-    }catch(err){
-      dispatchIfNotCancelled({type: 'ERROR', payload: err.message})
+    } 
+    catch (error) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: error })
       return null
     }
-
   }
 
   useEffect(() => {
